699922e20ac4d3db5fed4a2a4c5529a8881ea8e6
rick royd aban <rickroydaban@gmail.com>
2013-07-27 00:51:05 -0700

    will require a wifi to proceed to application

--- gen/abanyu/transphone/client/R.java
+++ gen/abanyu/transphone/client/R.java
@@ -139,16 +139,17 @@ containing a value of this type.
         public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int confirm=0x7f020000;
-        public static final int exit=0x7f020001;
-        public static final int gps=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int na=0x7f020004;
-        public static final int network=0x7f020005;
+        public static final int client=0x7f020000;
+        public static final int confirm=0x7f020001;
+        public static final int exit=0x7f020002;
+        public static final int gps=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int na=0x7f020005;
+        public static final int network=0x7f020006;
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f040005;
-        public static final int action_settings=0x7f04000b;
+        public static final int action_settings=0x7f04000d;
         public static final int confirmButton=0x7f040007;
         public static final int exitButton=0x7f040008;
         public static final int hybrid=0x7f040004;
@@ -156,13 +157,16 @@ containing a value of this type.
         public static final int none=0x7f040000;
         public static final int normal=0x7f040001;
         public static final int provImg=0x7f040009;
+        public static final int reloadButton=0x7f04000b;
         public static final int satellite=0x7f040002;
         public static final int terrain=0x7f040003;
-        public static final int text=0x7f04000a;
+        public static final int text=0x7f04000c;
+        public static final int tv=0x7f04000a;
     }
     public static final class layout {
         public static final int client_map=0x7f030000;
-        public static final int taxi_info=0x7f030001;
+        public static final int enablewifi=0x7f030001;
+        public static final int taxi_info=0x7f030002;
     }
     public static final class menu {
         public static final int client=0x7f080000;
